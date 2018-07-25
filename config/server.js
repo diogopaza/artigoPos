1cb57d212705a4c80f7e1b56f318e028a4a3e8b1
Diogo Paza <diogopazacvel@gmail.com>
2018-07-25 14:41:17 -0300

    cont bodyparser

--- config/server.js
+++ config/server.js
@@ -1,10 +1,28 @@
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set('views', '../views');
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(express.static( '../public'));
+var indexRouter = require('../routes/index');
+var usersRouter = require('../routes/users');
 
 
-app.use(express.static( './public'));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+const PORT = process.env.PORT || 3000
+
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na ${PORT}`)
+})
+
 
 module.exports = app
