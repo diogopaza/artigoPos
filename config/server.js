b6021bbd7004dad409615effedd259c79ffe0cf0
Diogo Paza <diogopazacvel@gmail.com>
2018-07-24 21:57:57 -0300

    index.js

--- config/server.js
+++ config/server.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
 
-const PORT = process.env.PORT || 3000
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na ${PORT}`)
-})
+app.use(express.static( './public'));
 
 module.exports = app
