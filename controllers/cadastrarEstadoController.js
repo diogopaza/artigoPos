b4b559f778c0aa3a9824ea36c32536de33eec9ef
Diogo Paza <diogopazacvel@gmail.com>
2018-07-25 22:43:48 -0300

    mongodb inserindo

--- controllers/cadastrarEstadoController.js
+++ controllers/cadastrarEstadoController.js
@@ -1,10 +1,21 @@
 var multiparty = require('multiparty')
+mongo = require('../config/dbConnection')
 
 module.exports.cadastrarEstado = function( req, res,next){
 
     var dadosForm = new multiparty.Form()
-    dadosForm.parse(req, function(err, campos, arquivos){
-      
+    dadosForm.parse(req, async function(err, campos, arquivos){
+      if(err) throw err
+        var db = mongo.getDbConnection()
+        collection = db.collection('estados')
+        await collection.insertMany([
+            {
+            name:'Diogo',
+            age:35
+            }
+        ])
+        console.log('usuario inserido com sucesso')
+
     })
    
    
